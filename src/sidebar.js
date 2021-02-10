6a3ea53d3c770d49c81c45b6a3bdb3d6422a4956
JolyneKo <jolynek4444@protonmail.com>
2021-02-10 19:03:00 -0400

    Javascript
    Adicionado Javascript e html modificado

---
+++ src/sidebar.js
@@ -0,0 +1,21 @@
+const menu1 = document.querySelector('.barra-superior .menu-icon');
+const menu2 = document.querySelector('.barra-lateral .menu-icon');
+const sidebar = document.querySelector('.barra-lateral');
+const icon = document.querySelector('.barra-superior img');
+
+menu1.addEventListener('click', e => {
+    sidebar.style.visibility = 'visible';
+    sidebar.style.width = '200px';
+    sidebar.style.padding = '10px 25px'; 
+});
+
+menu2.addEventListener('click', e => {
+    sidebar.style.visibility = 'hidden';
+    sidebar.style.width = '0';
+    sidebar.style.padding = '10px 0';
+});
+
+icon.addEventListener('click', e =>{
+    icon.style.transform = 'scaleX(-1)';
+    setTimeout(() => icon.style.transform = 'scaleX(1)', 300);
+});
